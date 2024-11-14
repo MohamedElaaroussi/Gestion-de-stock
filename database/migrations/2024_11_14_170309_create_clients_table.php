fcf35739bcb236cd0b90b305088290cf94e0462f
mohamedelaarousi <mohamedaitlaaroussi1@gmail.com>
2024-11-14 12:50:30 -0800

    first commit

---
+++ database/migrations/2024_11_14_170309_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id'); // Utilisez increments pour les versions antérieures de Laravel
+            $table->string('NomC');
+            $table->string('email')->unique();
+            $table->string('Téléphone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
